00343ec53afdd90ee6f19b6c87af49354dac467d
minsu <wjdalstn8443@naver.com>
2023-04-17 14:01:51 +0900

    start2

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>React World!</title>
+    <title>React World!!</title>
     <script src="https://unpkg.com/react@15.3.2/dist/react.js"></script>
     <script src="https://unpkg.com/react-dom@15.3.2/dist/react-dom.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.23/browser.min.js"></script>
@@ -14,7 +14,7 @@
         ReactDOM.render(
             <h1>
             <?php
-    echo "rrrr";
+    echo "rrrrrrr";
             ?>          
             </h1>,
             document.body

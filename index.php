f7b90586db050edaa4fbde5f4661684eda79949f
minsu <wjdalstn8443@naver.com>
2023-04-20 12:15:10 +0900

    check2

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
         ReactDOM.render(
             <h1>
             <?php
-    echo "rrrrrrr";
+        echo "rrrrrrr";
             ?>          
             </h1>,
             document.body

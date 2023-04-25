238d48b444d52910bbc7464f11f8efd99c4343ad
minsu <wjdalstn8443@naver.com>
2023-04-25 16:05:28 +0900

    0425

--- index.php
+++ index.php
@@ -7,21 +7,28 @@
     <script src="https://unpkg.com/react@15.3.2/dist/react.js"></script>
     <script src="https://unpkg.com/react-dom@15.3.2/dist/react-dom.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.23/browser.min.js"></script>
+    <link rel="stylesheet" href = "css/main.css">
 </head>
 <body>
     
     <script type="text/babel">
         ReactDOM.render(
-            <h1>
+            
             <?php
-
-
-        echo "2";
-        echo "1";
-        echo "3";
-
-            ?>          
-            </h1>,
+            ?>  
+            <div>
+                <div className = "header_wrap">
+                    <div className>
+                    </div>
+                    <div className = "gnb">
+                        <a href = "login.php">관리자페이지 </a>
+                    </div>
+                </div>
+                <div className = "block_img_div">
+                    <img className = "block_img" src="img/block.jpeg" ></img>
+                </div>
+            </div>
+            ,
             document.body
         );
     </script>

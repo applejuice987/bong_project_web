de27e4371d4c85db5eb732a9c833788e232be7d4
minsu <wjdalstn8443@naver.com>
2023-05-03 15:45:17 +0900

    ww

--- adminpage.php
+++ adminpage.php
@@ -4,26 +4,26 @@
 <head>
     <meta charset="UTF-8">
     <title>차단우회사이트</title>
-    <script src="https://unpkg.com/react@15.3.2/dist/react.js"></script>
-    <script src="https://unpkg.com/react-dom@15.3.2/dist/react-dom.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.23/browser.min.js"></script>
     <link rel="stylesheet" href = "css/admin.css"> 
     <link rel="stylesheet" href = "css/default.css">
+    <link rel="stylesheet" href = "css/codecreate.css">
+
     <style>
-          nav{
-            width: 390px;
-            height: 68px;
-            margin: 130px auto;
-            text-align: center;
-            background-color:navy;
-            line-height: 68px;
-        }  
+        nav{ width: 390px; height: 68px; margin: 130px auto; text-align: center; background-color:black; line-height: 68px; }  
+        .menu{position: absolute;top: 200px;left: 50px;padding: 60px 50px 0 0;}
+        .menu h2{padding: 0px 0px 20px 0;margin: 0px 0px 40px 0;border-bottom: 2px solid #000;text-align: center;}
+        .menu li{height: 65px; margin: 0 0px 0px 20px;}
+        .menu .border-right{border-right:1px solid #acacac;height: 800px;width: 250px;position: absolute; z-index: -999; top: 0px;}
+        .active{margin: 0 0px 0px 10px !important;}
+        .active a{background-color: #0c82e9;color: white;padding: 13px 14px 14px 10px;}
+        form{position:absolute;left: 50%; top: 600px;transform: translateX(-50%);}
+        form input:last-child{height: 35px;width: 60px;line-height: 35px;text-align: center;font-weight: 700;background-color: #0f59a5;color: #fff;padding: 0;font-size: 18px;border-radius: 3px;border: none;outline: none;}
+
     </style>
 </head>
 <body>
     
-    <script type="text/babel">
-        ReactDOM.render(
+    
             <div>
             <?php
             session_start();
@@ -35,29 +35,37 @@
             }
             else
             {
+                $forward = $_GET['forward'];
+                if ($forward==NULL)
+                    $forward==0;
+
+                
             ?> 
             <header>
-		        <div className="header_wrap">
-			        <div className="gnb">
+		        <div class="header_wrap">
+			        <div class="gnb">
 				        환영합니다!
 				
 			        </div>
 		        </div>
 		        <nav>
-			        <div className="lnb">
+			        <div class="lnb">
 				        <h2>관리자 페이지</h2>
 			        </div>
 		        </nav>
             </header>
-             <h6>관리자페이지</h6>
-        
+            <ul class="menu">
+	            <div class="border-right"></div>
+			        <h2>메뉴</h2>
+			            <li <?php if($forward==0) { ?> class= "active"  <?php } ?> ><a href="adminpage.php?forward=0"> 차단도메인관리</a></li>
+			            <li <?php if($forward==1) { ?> class= "active"  <?php } ?> ><a href="adminpage.php?forward=1"> 웹접속ip기록</a></li>
+
+            </ul>
+             
             <?php
             }
             ?>
             </div>
-            ,
-            document.body
-        );
-    </script>
+            
 </body>
 </html>
--- index.php
+++ index.php
@@ -4,39 +4,25 @@
 <head>
     <meta charset="UTF-8">
     <title>차단우회사이트</title>
-    <script src="https://unpkg.com/react@15.3.2/dist/react.js"></script>
-    <script src="https://unpkg.com/react-dom@15.3.2/dist/react-dom.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.8.23/browser.min.js"></script>
     <link rel="stylesheet" href = "css/main.css"> 
     <link rel="stylesheet" href = "css/default.css">
 </head>
 <body>
-    
-    <script type="text/babel">
-        ReactDOM.render(
-            
-            <?php
-            ?>  
             <div>
                 <header>
-                    <div className = "header_wrap">
-                        <div className = "gnb">
-                            <a className = "mmove" href = "login.php">관리자페이지 </a>
+                    <div class = "header_wrap">
+                        <div class = "gnb">
+                            <a class = "mmove" href = "login.php">관리자페이지 </a>
                         </div>
-                        
-                        
                     </div>
                     <div >
-                            <h1 className="logo">유해 정보 및 관리자에 의해 차단되었습니다.</h1>
+                            <h1 class="logo">유해 정보 및 관리자에 의해 차단되었습니다.</h1>
                     </div>
                 </header>
-                <div className = "block_img_div">
-                    <img className = "block_img" src="img/block.jpeg" ></img>
+                <div class = "block_img_div">
+                    <img class = "block_img" src="img/block.jpeg" ></img>
                 </div>
             </div>
-            ,
-            document.body
-        );
-    </script>
+            
 </body>
 </html>

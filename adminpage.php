6650165122943261820fb049df99aacc5da51c00
minsu <wjdalstn8443@naver.com>
2023-05-15 15:20:24 +0900

    tt

--- adminpage.php
+++ adminpage.php
@@ -1,4 +1,9 @@
 
+<?php
+    $forward = $_GET['forward'];
+    if ($forward==NULL)
+        $forward==0;
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,7 +12,8 @@
     <link rel="stylesheet" href = "css/admin.css"> 
     <link rel="stylesheet" href = "css/default.css">
     <link rel="stylesheet" href = "css/codecreate.css">
-
+    
+    
     <style>
         nav{ width: 390px; height: 68px; margin: 130px auto; text-align: center; background-color:black; line-height: 68px; }  
         .menu{position: absolute;top: 200px;left: 50px;padding: 60px 50px 0 0;}
@@ -18,13 +24,22 @@
         .active a{background-color: #0c82e9;color: white;padding: 13px 14px 14px 10px;}
         form{position:absolute;left: 50%; top: 600px;transform: translateX(-50%);}
         form input:last-child{height: 35px;width: 60px;line-height: 35px;text-align: center;font-weight: 700;background-color: #0f59a5;color: #fff;padding: 0;font-size: 18px;border-radius: 3px;border: none;outline: none;}
+        
+        <?php
+            if($forward == 1)
+            {
+        ?>
+        section h1{margin: 84px 0 108px 0 ; text-align: center;font-size: 4.8rem;}
 
+        <?php
+            }
+        ?>
     </style>
 </head>
 <body>
     
     
-            <div>
+        <div>
             <?php
             session_start();
             if($_SESSION['logined']==NULL){
@@ -35,7 +50,7 @@
             }
             else
             {
-                $forward = $_GET['forward'];
+                
                 if ($forward==NULL)
                     $forward==0;
 
@@ -58,14 +73,26 @@
 	            <div class="border-right"></div>
 			        <h2>메뉴</h2>
 			            <li <?php if($forward==0) { ?> class= "active"  <?php } ?> ><a href="adminpage.php?forward=0"> 차단도메인관리</a></li>
-			            <li <?php if($forward==1) { ?> class= "active"  <?php } ?> ><a href="adminpage.php?forward=1"> 웹접속ip기록</a></li>
+			            <li <?php if($forward==1) { ?> class= "active"  <?php } ?> ><a href="adminpage.php?forward=1"> 유해ip접속기록</a></li>
 
             </ul>
-             
+            
+            <section>
+
+            <?php
+                switch($forward)
+                {
+
+                    case 1:
+                        include 'ip_list.php';
+                }
+            ?>
+            </section>
+
             <?php
             }
             ?>
-            </div>
+        </div>
             
 </body>
 </html>
---
+++ ip_list.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+    </head>
+    <body>
+        <h1>
+            접근 시도하였던 유해 ip리스트
+        </h1>
+
+        <table width=300 cellpadding=1 cellspacing=0>
+        </table>
+    </body>
+</html>
